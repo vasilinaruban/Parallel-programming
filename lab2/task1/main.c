3affd08566b4aa622f0f13153326468b335f767d
vasilinaruban <vasilina.rub@mail.ru>
2024-04-15 17:17:49 +0700

    Add 2.1

---
+++ lab2/task1/main.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+
+double cpuSecond()
+{
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
+}
+
+/*
+ * matrix_vector_product: Compute matrix-vector product c[m] = a[m][n] * b[n]
+ */
+void matrix_vector_product(double *a, double *b, double *c, size_t m, size_t n)
+{
+    double sum = 0.0;
+    for (int i = 0; i < m; i++)
+    {
+        c[i] = 0.0;
+        for (int j = 0; j < n; j++)
+            c[i] += a[i * n + j] * b[j];
+            sum += c[i];
+    }
+    //printf("serial: %lf\n", sum);
+}
+
+// void matrix_vector_product_omp(double *a, double *b, double *c, size_t m, size_t n)
+// {
+// #pragma omp parallel
+//     {
+//         double t = omp_get_wtime();
+//         int nthreads = omp_get_num_threads();
+//         int threadid = omp_get_thread_num();
+//         int items_per_thread = m / nthreads;
+//         int lb = threadid * items_per_thread;
+//         int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+//         for (int i = lb; i <= ub; i++)
+//         {
+//             c[i] = 0.0;
+//             for (int j = 0; j < n; j++)
+//                 c[i] += a[i * n + j] * b[j];
+//         }
+//         t = omp_get_wtime() - t;
+//         printf("Thread %d items %d [%d - %d], time: %.6f\n", threadid, ub - lb + 1, lb, ub, t);
+//     }
+// }
+
+/*
+    matrix_vector_product_omp: Compute matrix-vector product c[m] = a[m][thread_num] * b[thread_num]
+*/
+
+int bound (int id, int remainder, int items_per_thread)
+{
+    int bound = 0;
+    if (id < remainder)
+    {
+        bound = id * (items_per_thread + 1);
+    }
+    else
+    {
+        bound = remainder * (items_per_thread + 1) + (id - remainder) * items_per_thread;
+    }
+    return bound;
+}
+
+void matrix_vector_product_omp(double *a, double *b, double *c, size_t m, size_t n)
+{
+    double sum = 0.0;
+#pragma omp parallel
+    {
+        int nthreads = omp_get_num_threads();
+        int threadid = omp_get_thread_num();
+        int k = m % nthreads;
+        int items_per_thread = m / nthreads;
+        int lb = bound(threadid, k, items_per_thread);
+        int ub = bound(threadid + 1, k, items_per_thread);
+
+        double local_sum = 0.0;
+        for (int i = lb; i < ub; i++)
+        {
+            c[i] = 0.0;
+            for (int j = 0; j < n; j++) {
+                c[i] += a[i * n + j] * b[j];
+            }
+            local_sum += c[i];
+        }
+#pragma omp atomic
+        sum += local_sum;
+
+    }
+    //printf("parallel: %lf\n", sum);
+}
+
+void run_serial(size_t n, size_t m)
+{
+    double *a, *b, *c;
+    a = (double*)malloc(sizeof(*a) * m * n);
+    b = (double*)malloc(sizeof(*b) * n);
+    c = (double*)malloc(sizeof(*c) * m);
+
+    if (a == NULL || b == NULL || c == NULL)
+    {
+        free(a);
+        free(b);
+        free(c);
+        printf("Error allocate memory!\n");
+        exit(1);
+    }
+
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+            a[i * n + j] = i + j;
+    }
+
+    for (size_t j = 0; j < n; j++)
+        b[j] = j;
+
+    double t = cpuSecond();
+    matrix_vector_product(a, b, c, m, n);
+    t = cpuSecond() - t;
+
+    printf("Elapsed time (serial): %.6f sec.\n", t);
+    free(a);
+    free(b);
+    free(c);
+}
+
+void run_parallel(size_t n, size_t m)
+{
+    double *a, *b, *c;
+
+    a = (double*)malloc(sizeof(*a) * m * n);
+    b = (double*)malloc(sizeof(*b) * n);
+    c = (double*)malloc(sizeof(*c) * m);
+
+    if (a == NULL || b == NULL || c == NULL)
+    {
+        free(a);
+        free(b);
+        free(c);
+        printf("Error allocate memory!\n");
+        exit(1);
+    }
+
+#pragma omp parallel for collapse(2)
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+            a[i * n + j] = i + j;
+    }
+
+#pragma omp parallel for
+    for (size_t j = 0; j < n; j++)
+        b[j] = j;
+
+
+    double t = cpuSecond();
+    matrix_vector_product_omp(a, b, c, m, n);
+    t = cpuSecond() - t;
+
+    printf("Elapsed time (parallel): %.6f sec.\n", t);
+    free(a);
+    free(b);
+    free(c);
+}
+
+int main(int argc, char *argv[])
+{
+    size_t M = 20000;
+    size_t N = 20000;
+    if (argc > 1)
+        M = atoi(argv[1]);
+    if (argc > 2)
+        N = atoi(argv[2]);
+    run_serial(M, N);
+    run_parallel(M, N);
+    return 0;
+}
