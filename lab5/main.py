3330b03e31d809a09631250ab89574ecceaf19b6
vasilinaruban <vasilina.rub@mail.ru>
2024-05-28 23:51:29 +0700

    Add lab6

--- lab5/main.py
+++ lab5/main.py
@@ -1,75 +1,70 @@
-import cv2
 import argparse
 import time
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Manager
+from multiprocessing import Event
+from queue import Queue
+from threading import Thread
+
+import cv2
 from ultralytics import YOLO
 
-class VideoCap:
-    def __init__(self, video_path):
-        self.video = cv2.VideoCapture(video_path)
 
-    def __enter__(self):
-        return self.video
+def parse():
+    parser = argparse.ArgumentParser(description="Запуск yolov8s-pose на нескольких потоках")
+    parser.add_argument("-t", "--num_threads", type=int, default=1,
+                        help="Количество потоков для многопоточного режима.")
+    parser.add_argument("video_path", type=str, help="Путь к входному видеофайлу.")
+    parser.add_argument("output_file", type=str, help="Имя выходного видеофайла.")
+    args = parser.parse_args()
+    return args.num_threads, args.video_path, args.output_file
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.video.release()
 
-class YOLOModel:
-    def __init__(self, model_path):
-        self.model_path = model_path
-        self.model = YOLO(self.model_path)
+def predict_work(in_queue, out_queue, stop_event):
+    model = YOLO("yolov8s-pose.pt")
+    while not stop_event.is_set():
+        frame = in_queue.get()
+        if frame is None:  
+            break
+        out_queue.put(model.predict(frame, verbose=False, device="cpu")[0].plot())
 
-    def predict(self, frame):
-        result = self.model(frame, device='cpu')[0].plot()
-        return result
-
-def process_frame(frame, model_path):
-    yolo_model = YOLOModel(model_path)
-    return yolo_model.predict(frame)
-
-class VideoProcessor:
-    def __init__(self, video_path, output_path):
-        self.video_path = video_path
-        self.output_path = output_path
-
-    def process_video(self, num_processes=1):
-        with VideoCap(self.video_path) as video:
-            fps = video.get(cv2.CAP_PROP_FPS)
-            num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-            frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-            frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-            out = cv2.VideoWriter(self.output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
-
-            frames = []
-            for i in range(num_frames):
-                ret, frame = video.read()
-                if not ret:
-                    break
-                frames.append(frame)
-
-            start_time = time.time()
-
-            with ProcessPoolExecutor(max_workers=num_processes) as executor:
-                results = list(executor.map(process_frame, frames, ['yolov8n-pose.pt'] * len(frames)))
-
-            for processed_frame in results:
-                cv2.imshow('Processed Frame', processed_frame)
-                cv2.waitKey(1)
-                out.write(processed_frame)
-
-            end_time = time.time()
-
-        return end_time - start_time
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process video with YOLOv8s-pose model.')
-    parser.add_argument('--video_path', type=str, help='Path to input video')
-    parser.add_argument('--output_path', type=str, help='Path to output video')
-    parser.add_argument('--num_processes', type=int, default=1, help='Number of processes for parallel processing')
-    args = parser.parse_args()
+    num_threads, video_path, output_file = parse()
 
-    processor = VideoProcessor(args.video_path, args.output_path)
-    execution_time = processor.process_video(args.num_processes)
-    print(f"Execution time: {execution_time} seconds")
+    start_time = time.time()
+
+    in_video = cv2.VideoCapture(video_path)
+
+    stop_event = Event()
+    in_queues = [Queue() for _ in range(num_threads)]
+    out_queues = [Queue() for _ in range(num_threads)]
+    threads = [Thread(target=predict_work, args=(in_queues[i], out_queues[i], stop_event)) for i in range(num_threads)]
+    for thread in threads:
+        thread.start()
+
+    num_frames = 0
+    while True:
+        success, frame = in_video.read()
+        if not success:
+            break
+        in_queues[num_frames % num_threads].put(frame)
+        num_frames += 1
+
+    for in_queue in in_queues:
+        in_queue.put(None)
+
+    out_video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), 30,
+                                (int(in_video.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                 int(in_video.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    for i in range(num_frames):
+        frame = out_queues[i % num_threads].get()
+        out_video.write(frame)
+
+    stop_event.set()
+    for thread in threads:
+        thread.join()
+    in_video.release()
+    out_video.release()
+
+    end_time = time.time()
+
+    print(f"Elapsed time: {round(end_time - start_time, 2)}")
